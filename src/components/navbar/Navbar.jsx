8488f99f99b3efcd71d1100252d6d23bde3ab5fd
SushantKhanal <dreamerking114@gmail.com>
2022-08-05 23:29:43 -0500

    sidebar toggle from navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,68 @@
+import "./navbar.scss";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import FullscreenExitOutlinedIcon from "@mui/icons-material/FullscreenExitOutlined";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
+import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
+import { DarkModeContext } from "../../context/darkModeContext";
+import { useContext, useState } from "react";
+import MenuIcon from "@mui/icons-material/Menu";
+
+const Navbar = () => {
+  const { dispatch } = useContext(DarkModeContext);
+  const [sidebarVisible, setSidebarVisible] = useState(true);
+
+  const sidebarToggleHandler = () => {
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = sidebarVisible ? "none" : "block";
+    const menuIcon = document.querySelector(".wrapper .menu .icon");
+    menuIcon.style.left = sidebarVisible ? "2%" : "12%";
+    setSidebarVisible(!sidebarVisible);
+  };
+
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="menu">
+          <MenuIcon className="icon" onClick={() => sidebarToggleHandler()} />
+        </div>
+        <div className="search">
+          <input type="text" placeholder="Search..." />
+          <SearchOutlinedIcon />
+        </div>
+        <div className="items">
+          <div className="item">
+            <DarkModeOutlinedIcon
+              className="icon"
+              onClick={() => dispatch({ type: "TOGGLE" })}
+            />
+          </div>
+          <div className="item">
+            <FullscreenExitOutlinedIcon className="icon" />
+          </div>
+          <div className="item">
+            <NotificationsNoneOutlinedIcon className="icon" />
+            <div className="counter">1</div>
+          </div>
+          <div className="item">
+            <ChatBubbleOutlineOutlinedIcon className="icon" />
+            <div className="counter">2</div>
+          </div>
+          <div className="item">
+            <ListOutlinedIcon className="icon" />
+          </div>
+          <div className="item">
+            <img
+              src="https://images.pexels.com/photos/941693/pexels-photo-941693.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+              alt=""
+              className="avatar"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
